42a1a4ab0940011072bc726eec81e45b5a288dca
KennySabee <kemerizalde.dwfsc10@ucamp.utel.edu.mx>
2023-02-03 09:47:07 -0500

    feat update files

--- src/routers/shopping.routers.js
+++ src/routers/shopping.routers.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-const { getShopping, createShopping, deleteShopping, updateShopping} = require ('../controllers/Shopping.controllers')
+const { getShopping, createShopping, deleteShopping, updateShopping} = require ('../controllers/shopping.controllers')
 
 
 //localhost:5000/Shopping
